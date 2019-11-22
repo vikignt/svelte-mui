278e28d9e6dcc4bcb67f64158b9af9786e1a469a
vikignt <vikignt@gmail.com>
2019-11-22 13:49:03 +0300

    button group: update padding button inside button group

--- src/ButtonGroup.svelte
+++ src/ButtonGroup.svelte
@@ -6,14 +6,22 @@
 	.button-group {
 		display: inline-table;
 	}
+	.button-group :global(button) {
+		padding: 0 8px;
+	}
 	.button-group :global(button:first-child) {
 		border-top-right-radius: 0;
 		border-bottom-right-radius: 0;
 	}
-
 	.button-group :global(button:last-child) {
 		border-top-left-radius: 0;
 		border-bottom-left-radius: 0;
+	}
+	.button-group :global(.shaped:first-child) {
+		padding-left: 12px;
+	}
+	.button-group :global(.shaped:last-child) {
+		padding-right: 12px;
 	}
 
 	.button-group :global(button:not(:first-child):not(:last-child)) {
