f820cef618217f9e4e9f7ab7b6859badea9e5167
vikignt <vikignt@gmail.com>
2019-11-26 01:49:34 +0300

    prettier format

--- example/src/pages/dialog/props.js
+++ example/src/pages/dialog/props.js
@@ -27,12 +27,14 @@ export default [
 		name: 'closeByEsc',
 		def: 'true',
 		type: 'boolean',
-		desc: 'To prevent the dialog from closing by <kbd>ESC</kbd> key, set it to <code>false</code>',
+		desc:
+			'To prevent the dialog from closing by <kbd>ESC</kbd> key, set it to <code>false</code>',
 	},
 	{
 		name: 'beforeClose',
 		def: '() => true',
 		type: 'function',
-		desc: 'Callback function starts before closing the dialog, returns <code>false</code> to prevent the dialog from closing',
+		desc:
+			'Callback function starts before closing the dialog, returns <code>false</code> to prevent the dialog from closing',
 	},
 ];
