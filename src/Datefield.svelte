4a426a7c99d23db08a8b81fe8fe4bb23f2e7aade
forceofseth <forceofseth@gmail.com>
2021-01-04 19:38:33 +0100

    fixes behavoir of event bubbling. (#41)
    Co-authored-by: Lukas Kiefer <lukas.kiefer@netcetera.com>

--- src/Datefield.svelte
+++ src/Datefield.svelte
@@ -127,12 +127,14 @@
 		text = tostring(detail, format);
 		value = typeof value === 'string' ? text : clone(detail);
 		visible = false;
-		readonly ? setval(text) : focusInputElm();
+		setval(text);
+		focusInputElm();
 	}
 
 	function onfocus() {
 		inputActive = true;
 		readonly && open();
+		dispatch('focus', e);
 	}
 
 	function onblur(e) {
@@ -143,6 +145,7 @@
 				return;
 			}
 			setval(text);
+			dispatch('blur', e);
 		}, 0);
 	}
 
